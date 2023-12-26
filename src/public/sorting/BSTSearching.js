8b566281e7c47eb66a2087c0f5942167b899c9a8
Parthiban K <41236775+parthibankrpa@users.noreply.github.com>
2023-12-26 11:59:20 +0530

    Create BSTSearching.js

---
+++ src/public/sorting/BSTSearching.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // Insert a new node with a given value into the BST
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // Helper function to insert a node recursively
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Search for a value in the BST
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  // Helper function to search for a value recursively
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value === node.value) {
+      return true;
+    } else if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+}
+
+// Example usage:
+const bst = new BinarySearchTree();
+
+// Insert elements into the BST
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+bst.insert(7);
+bst.insert(12);
+bst.insert(17);
+
+// Search for values in the BST
+console.log(bst.search(12)); // Output: true
+console.log(bst.search(6));  // Output: false
